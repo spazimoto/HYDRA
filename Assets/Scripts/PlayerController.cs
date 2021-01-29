7f4b39a09767eb5c075d2bb5de28a93eb24ff380
Morgan <morgan_demunck@yahoo.com>
2021-01-29 15:02:01 -0500

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = .1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float yDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, yDirection, 0.0f);
+
+        transform.position += moveDirection * speed;
+    }
+}
